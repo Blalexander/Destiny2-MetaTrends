96c32b04731dc05d21ed2cfa1fdaa6201cfbeacb
Blake Alexander <blake.alexander.13@gmail.com>
2020-05-14 11:32:57 -0700

    updates for azure

--- src/config.js
+++ src/config.js
@@ -3,7 +3,6 @@ module.exports = {
   MONGODB_URI: process.env.MONGODB_URI || 'mongodb://testuser:testpassword@ds127015.mlab.com:27015/react-capstone',
   JWT_SECRET: process.env.JWT_SECRET,
   JWT_EXPIRY: process.env.JWT_EXPIRY || '7d',
-  // API_BASE_URL: process.env.API_URL || 'https://enigmatic-savannah-35083.herokuapp.com/',
-  // use line 9 if working locally, line 7 if deploying
-  API_BASE_URL: 'http://localhost:8080/'
+  API_BASE_URL: process.env.API_URL || 'https://enigmatic-savannah-35083.herokuapp.com/',
+  // API_BASE_URL: 'http://localhost:8080/'
 }; 
